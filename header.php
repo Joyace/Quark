a2be53930814808914fe3d2dda95cc60f5c1d9ad
Joyace <605036335@qq.com>
2018-09-26 23:46:29 +0800

    修改了字体和一些细节

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 <style type="text/css">
 *{margin:0;padding:0;border:0;max-width:100%!important;word-wrap:break-word;word-break:break-all}
-body{font-family:Helvetica,"Microsoft YaHei",sans-serif;font-size:.9375rem;line-height:2;color:#444;background:#fff;letter-spacing:1px}
+body{font-family:Tahoma,"Hiragino Sans GB","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;font-size:.9375rem;line-height:2;color:#000;background:#fdfdfd;letter-spacing:1px}
 blockquote{padding:0 15px;color:#666;border-left:4px solid #ddd}
 pre{background:#f7f7f7;margin:1.25rem 0;padding:.9375rem;overflow:auto;color:#4d4d4c;line-height:1.75}
 pre code{background:0 0;text-shadow:none;padding:0;margin:0}
@@ -12,15 +12,16 @@ code{background:#eee;text-shadow:0 1px #fff;padding:0 .25rem;margin:0 .25rem;col
 embed{display: none;}
 hr{margin:40px 0;height:1px;background-color:#ddd}
 ol,ul{list-style:none}
-a{color:#555;text-decoration:none}
-a.active,a:hover{color:#000;outline:0;transition:.2s ease-in-out}
+a{color:#000;text-decoration:none}
+a.active,a:hover{color:#666;outline:0;transition:.3s ease-in-out}
 p{margin:0 0 25px 0}
 h1,h2,h3,h4,h5,h6{font-weight:400;margin:1.25rem 0 .625rem}
 h1{font-size:1.625rem;line-height: 1.25;margin:0}
 h2{font-size:1.375rem}
 h3{font-size:1.1875rem}
 h4{font-size:1rem;border-left:3px solid #777;padding-left:8px}
-#h{height:124px;width:100%;background:#f5f5f5;border-bottom:1px solid #e1e2e3;font-size:.8125rem;margin:0 auto}
+
+#h{height:124px;width:100%;background:#fff;border-bottom:1px solid #e1e2e3;font-size:.8125rem;margin:0 auto}
 #comments,.p{padding:0 1.25rem}
 .w1{width:768px}
 #nav{margin:auto;position:relative;height:100%}
@@ -31,7 +32,8 @@ h4{font-size:1rem;border-left:3px solid #777;padding-left:8px}
 #nav ul{position:absolute;right:10px;top:51px}
 #nav ul li{display:inline-block;float:left}
 #nav ul a{display:block;padding:2px .625rem}
-#nav ul a:hover{background:#ddd;text-decoration:none}
+
+#nav ul a:hover{background:#eee;text-decoration:none}
 #m{margin:auto;padding-top:.625rem}
 .p{margin:4.2rem 0}
 .p ol,.p ul{list-style-type:circle;padding-left:1.5rem;margin-bottom:1.25rem}
@@ -43,16 +45,17 @@ h4{font-size:1rem;border-left:3px solid #777;padding-left:8px}
 .pp{margin:1.5rem 0;text-align:justify;text-justify:inter-ideograph}
 .pp img{display:block;margin:.875rem 0;max-width:100%;height:auto}
 .pp h2,.pp h3,.pp h4{padding:.625rem 0 .9375rem}
-.s .pp a:before{content:' Link';vertical-align:super;color:#b00;font-size:.5625rem}
-.s .pp a{border-bottom:1px dashed #c00}
+.s .pp a{border-bottom:1px dashed #f00}
 .s .pp a:hover{border-bottom:1px solid #aaa}
+
+
 .album img{display:block;max-width:75%;width:75%;margin:.875rem auto}
 .ptg{margin:2rem 0 3rem 0}
-.ptg a{font-size:.8125rem;margin-right:10px;background:#f5f5f5;padding:2px 6px}
+.ptg a{font-size:.8125rem;margin-right:10px;background:#fff;padding:2px 6px}
 .ptg a:before{content:'# '}
 .ptg a:hover{background:#ccc}
 .pnv{overflow:hidden;margin-top:60px;padding:10px;white-space:nowrap;border-top:1px solid #eee}
-.pnv a:hover{font-weight:700}
+.pnv a:hover{font-weight:500}
 .pnv .next,.pnv .prev{display:inline-block;width:50%;white-space:normal}
 .pnv .prev{text-align:left}
 .pnv .next{text-align:right}
@@ -60,7 +63,8 @@ h4{font-size:1rem;border-left:3px solid #777;padding-left:8px}
 .page-navigator li{display:inline-block;margin-right:1px}
 .page-navigator li a{padding:5px 1rem}
 .page-navigator li a:hover,.page-navigator li.current a{background:#ddd;text-decoration:none;border-bottom:1px solid #aaa}
-#f{border-top:1px solid #e1e2e3;padding:3.5rem 0;text-align:center;background:#f5f5f5;font-size:.8125rem}
+
+#f{border-top:1px solid #e1e2e3;padding:3.5rem 0;text-align:center;background:#fff;font-size:.8125rem}
 @media only screen and (max-width:767px){.pm{margin-top:1.1rem}
 h1{font-size:1.375rem}
 h2{font-size:1.25rem}
@@ -102,7 +106,7 @@ h3{font-size:1.125rem}
 <div id="nav" class="w1">
     <a href="#" class="x" onclick = "tg_c('h','o');"><div><span class="t"></span><span class="m"></span><span class="b"></span></div></a>
     <div class="logo"><a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a></div>
-    <ul><li><a href="<?php $this->options->siteUrl(); ?>">首页</a></li><?php $this->widget('Widget_Contents_Page_List')->to($pages); ?><?php while($pages->next()): ?><li><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li><?php endwhile; ?></ul>
+    <ul><?php $this->widget('Widget_Contents_Page_List')->to($pages); ?><?php while($pages->next()): ?><li><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li><?php endwhile; ?></ul>
 </div>
 </div>
 <div id="m" class="w1">
